b46880be86db0206f878d07aa28ebcb152fe2f3e
TBS96 <prantik.g1996@gmail.com>
2024-08-07 03:58:48 +0530

    added views for data rendering; completed project
    Signed-off-by: TBS96 <prantik.g1996@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native'
+import { FlatList, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import { currencyByRupee } from './constants'
 import CurrencyBtn from './components/CurrencyBtn'
@@ -18,7 +18,7 @@ const App = () => {
     {
       return Snackbar.show({
         text: 'Enter a value to convert!!!',
-        backgroundColor: '#4fe90883',
+        backgroundColor: '#ff0000',
         textColor: '#ffffff'
       })
     }
@@ -34,9 +34,10 @@ const App = () => {
     else
     {
       return Snackbar.show({
-        text: 'Not a valid number to convert! Pl. enter a valid number',
+        text: 'Not a valid number to convert! Please enter a valid number!!',
         backgroundColor: '#F4BE2C',
-        textColor: '#ffffff'
+        textColor: '#ffffff',
+        duration: Snackbar.LENGTH_LONG
       })
     }
 
@@ -45,8 +46,43 @@ const App = () => {
   return (
     <>
       <StatusBar />
-      <View>
-        <Text>App</Text>
+      <View style = {styles.container}>
+        
+        <View style = {styles.topContainer}>
+          <View style = {styles.rupeesContainer}>
+            <Text style = {styles.rupee}>₹</Text>
+            <TextInput 
+              style = {styles.textInput}
+              maxLength = {20}
+              value = {inputVal}
+              onChangeText={setInputVal}
+              keyboardType='number-pad'
+              keyboardAppearance='default'
+              placeholder='Enter amount in ₹...'
+            />
+          </View>
+          {resultVal && (
+            <Text style = {styles.resultTxt}>
+              {resultVal}
+            </Text>
+          )}
+        </View>
+
+        <View style = {styles.bottomContainer}>
+          <FlatList 
+            numColumns={2}
+            data={currencyByRupee}
+            keyExtractor={item => item.name}
+            renderItem={({item}) => (
+              <TouchableOpacity
+                style = {[styles.button, targetCurrency === item.name && styles.selected]}
+                onPress={() => btnPressed(item)}>
+                  <CurrencyBtn {...item}/>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+
       </View>
     </>
   )
@@ -57,62 +93,68 @@ export default App
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#515151',
+    backgroundColor: '#5d89c2',
   },
   topContainer: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-evenly',
+    justifyContent: 'center',
+    backgroundColor: '#07ec1aa1',
+    marginVertical: 10,
+    marginStart: 25,
+    marginEnd: 25,
+    borderTopEndRadius: 35,
+    borderBottomStartRadius: 35,
+    borderTopStartRadius: 5,
+    borderBottomEndRadius: 5
   },
   textInput: {
     padding: 8,
-    width: '30%',
+    width: 270,
     borderWidth: 2,
-    borderRadius: 4,
-    borderColor: '#DF8524',
+    borderRadius: 20,
+    borderColor: 'yellow',
+    fontSize: 18,
+    fontFamily: 'monospace'
   },
   resultTxt: {
     fontSize: 32,
-    color: '#000000',
+    color: 'orange',
     fontWeight: '800',
+    borderWidth: 2,
+    borderRadius: 50,
+    padding: 20,
+    marginTop: 25
   },
   rupee: {
     marginRight: 8,
     fontSize: 22,
-    color: '#000000',
+    color: 'gold',
     fontWeight: '800',
   },
   rupeesContainer: {
     flexDirection: 'row',
     alignItems: 'center',
   },
-  inputAmountField: {
-    height: 40,
-    width: 200,
-    padding: 8,
-    borderWidth: 1,
-    borderRadius: 4,
-    backgroundColor: '#FFFFFF',
-  },
   bottomContainer: {
-    flex: 3,
+    flex: 1,
   },
   button: {
     flex: 1,
-    margin: 12,
-    height: 60,
+    margin: 10,
+    height: 80,
     borderRadius: 12,
-    backgroundColor: '#fff',
-    elevation: 2,
+    backgroundColor: 'wheat',
+    elevation: 5,
     shadowOffset: {
       width: 1,
       height: 1,
     },
-    shadowColor: '#333',
-    shadowOpacity: 0.1,
-    shadowRadius: 1,
+    shadowColor: 'red',
+    shadowOpacity: 1,
+    shadowRadius: 10,
   },
   selected: {
-    backgroundColor: '#ffeaa7',
+    backgroundColor: '#7cd46a',
   },
 })
