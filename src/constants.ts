8d4f76d887b1447b1d1b92dd65116c5eef29d4f9
TBS96 <prantik.g1996@gmail.com>
2024-08-07 01:08:12 +0530

    create index.d.ts and define an interface Currency with properties: name, value, flag, symbol & a file named constants.ts where we export currencyByRupee[] array having objects of different country details
    Signed-off-by: TBS96 <prantik.g1996@gmail.com>

---
+++ src/constants.ts
@@ -0,0 +1,80 @@
+export const currencyByRupee: Currency[] = [
+    {
+        name: 'DOLLAR',
+        value: 0.012,
+        flag: '🇺🇸',
+        symbol: '$',
+    },
+    {
+        name: 'EURO',
+        value: 0.011,
+        flag: '🇪🇺',
+        symbol: '€',
+    },
+    {
+        name: 'POUND STERLING',
+        value: 0.0094,
+        flag: '🇬🇧',
+        symbol: '£',
+    },
+    {
+        name: 'RUBEL',
+        value: 1.02,
+        flag: '🇷🇺',
+        symbol: '₽',
+    },
+    {
+        name: 'AUS DOLLAR',
+        value: 0.018,
+        flag: '🇦🇺',
+        symbol: 'A$',
+    },
+    {
+        name: 'CAN DOLLAR',
+        value: 0.016,
+        flag: '🇨🇦',
+        symbol: 'C$',
+    },
+    {
+        name: 'YEN',
+        value: 1.73,
+        flag: '🇯🇵',
+        symbol: '¥',
+    },
+    {
+        name: 'HONG KONG DOLLAR',
+        value: 0.093,
+        flag: '🇭🇰',
+        symbol: 'HKD',
+    },
+    {
+        name: 'KUWAITI DINAR',
+        value: 0.0036,
+        flag: '🇰🇼',
+        symbol: 'KWD',
+    },
+    {
+        name: 'UNITED ARAB EMIRATES DIRHAM',
+        value: 0.044,
+        flag: '🇦🇪',
+        symbol: 'UAE',
+    },
+    {
+        name: 'BDT',
+        value: 1.40,
+        flag: '🇧🇩',
+        symbol: '৳',
+    },
+    {
+        name: 'PKR',
+        value: 3.33,
+        flag: '🇵🇰',
+        symbol: 'Rs.',
+    },
+    {
+        name: 'BITCOIN',
+        value: 0.000000543544886,
+        flag: '🎰',
+        symbol: '₿',
+    }
+];
